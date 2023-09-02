a94ff30ae79aa331bb085738ea34eadfe9da72c1
Gerald Alinio <38755420+imgeraldalinio@users.noreply.github.com>
2023-09-02 09:05:10 +0800

    Update obtain-ssl-cert.sh

--- obtain-ssl-cert.sh
+++ obtain-ssl-cert.sh
@@ -1,56 +1,49 @@
 #!/bin/bash
 
-# checking domain name
-if [ $# -lt 1 ]
-then
-    echo "At least one or more domain name is required: (e.g example.com www.example.com)"
+# Check for domain names as arguments
+if [ $# -lt 1 ]; then
+    echo "At least one or more domain names are required (e.g., example.com www.example.com)"
     exit 1
 fi
 
-# Concat the requested domains
-DOMAINS=""
-for i in "$@"
-do
-    DOMAINS+=" -d $i"
+# Concatenate the requested domains
+DOMAINS=("$@")
+
+# Extract root domain (e.g., example.com) from the list
+root_domain=""
+for domain in "${DOMAINS[@]}"; do
+    if [[ $domain =~ ([a-zA-Z0-9-]+\.[a-zA-Z]{2,24}) ]]; then
+        root_domain="${BASH_REMATCH[1]}"
+        break
+    fi
 done
 
-# filter domain name
-domain_name=$(printf "$DOMAINS" | grep -oE '[a-z0-9][a-z0-9-]{0,61}[a-z0-9](\w{2,}\.\w{2,3}\.\w{2,3}|\w{1,}\.\w{2,24})$')
+# Prepare certbot command
+certbot_command="certbot certonly --config /etc/letsencrypt/cli.ini -d $root_domain"
 
-# dns challenge
-# certbot certonly --config /etc/letsencrypt/cli.ini  --manual --preferred-challenges dns $DOMAINS --cert-name $domain_name
+# Add additional domains to the certbot command
+for domain in "${DOMAINS[@]}"; do
+    certbot_command+=" -d $domain"
+done
 
-matched_root_domain=`echo $DOMAINS | grep -F -- "-d $domain_name" | wc -l`
-if [ $matched_root_domain -eq 1 ]
-then
-        # remove root domain from $DOMAINS
-        remove_root_domain=`echo $DOMAINS | sed 's/-d '$domain_name'//g'`
+# Execute the certbot command
+echo "Running Certbot command: $certbot_command"
+eval "$certbot_command"
 
-        # http-01 challenge
-        echo "Certbot command 1"
-        certbot certonly --config /etc/letsencrypt/cli.ini -d $domain_name $remove_root_domain --cert-name $domain_name
-else
-        echo "Certbot command 2"
-        certbot certonly --config /etc/letsencrypt/cli.ini -d $domain_name $DOMAINS --cert-name $domain_name
+# Check if SSL certificate was obtained successfully
+if [ ! -d "/etc/letsencrypt/live/$root_domain" ]; then
+    echo "Failed to obtain SSL certificate!"
+    exit 1
 fi
 
+# Copy Let's Encrypt certificate to HAProxy certs folder
+cert_path="/etc/letsencrypt/live/$root_domain"
+cat "$cert_path/fullchain.pem" "$cert_path/privkey.pem" > "/etc/haproxy/certs/$root_domain.pem"
+chmod 600 "/etc/haproxy/certs/$root_domain.pem"
 
-if [ ! -d "/etc/letsencrypt/live/$domain_name" ]
-then
-        echo "Failed to obtain SSL certificate!"
-else
-        # Remove readme file if exists to avoid error
-        if [ -f /etc/letsencrypt/live/README ]
-        then
-                rm /etc/letsencrypt/live/README
-        fi
-        # Copy letsencrypt certificate and place to haproxy certs folder
-        cat /etc/letsencrypt/live/$domain_name/{fullchain.pem,privkey.pem} > /etc/haproxy/certs/$domain_name.pem
-        chmod 600 /etc/haproxy/certs/$domain_name.pem
+# Reload HAProxy
+/etc/init.d/haproxy reload
 
-        sleep 1
-        # reload haproxy
-        /etc/init.d/haproxy reload
-fi
+echo "SSL certificate obtained and applied successfully for $root_domain."
 
 exit 0
